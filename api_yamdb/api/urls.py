a445046366e346efa4d833ea552975ce2d7c8f25
Petr Lidzhiev <petyaposk@gmail.com>
2023-07-09 13:15:12 +0300

    fix

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,12 +2,14 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views import (CategoryViewSet, CommentViewSet, GenreViewSet,
-                       ReviewViewSet, TitleViewSet)
+                       ReviewViewSet, TitleViewSet, UserViewSet,
+                       SignUpView, GetTokenView)
 
 apps_name = 'api'
 
-router = DefaultRouter()
 
+router = DefaultRouter()
+router.register('v1/users', UserViewSet, basename='users')
 router.register('v1/categories', CategoryViewSet, basename='categories',)
 router.register('v1/genres', GenreViewSet, basename='genres')
 router.register('v1/titles', TitleViewSet, basename='titles')
@@ -18,5 +20,7 @@ router.register(r'title/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
 
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('v1/auth/signup/', SignUpView.as_view(), name='signup'),
+    path('v1/auth/token/', GetTokenView.as_view(), name='token'),
+    path('v1/', include(router.urls)),
 ]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import get_object_or_404
+from django.conf import settings
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
-
-
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.tokens import default_token_generator
+from rest_framework import status, viewsets
+from django.db import IntegrityError
+from rest_framework.response import Response
+from django.core.mail import send_mail
 from reviews.models import Category, Genre, Titles, Review, User
+from rest_framework_simplejwt.tokens import AccessToken
 from .permissions import AdminOnly, IsAdminUserOrReadOnly, \
     IsAuthenticatedOrReadOnly
 from .serializers import CategorySerializer, GenreSerializer, \
-    TitlesSerializer, CommentSerializer, ReviewSerializer, UserSerializer
+    TitlesSerializer, CommentSerializer, ReviewSerializer, UserSerializer, GetTokenSerializer, SignUpSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -69,3 +74,75 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
         serializer.save(author=self.request.user, review=review)
+
+
+class GetTokenView(APIView):
+    """Вьюсет для создания токена."""
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        serializer = GetTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        confirmation_code = serializer.validated_data.get('confirmation_code')
+        user = get_object_or_404(
+            User, username=serializer.validated_data['username'],
+        )
+        if user.is_activated:
+            return Response(
+                'Пользователь уже активирован.',
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        if not default_token_generator.check_token(user, confirmation_code):
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        user.is_activated = True
+        user.save()
+        token = AccessToken.for_user(user)
+        return Response({'token': str(token)}, status=status.HTTP_200_OK)
+
+
+class SignUpView(APIView):
+    """Вьюсет для регистрации."""
+    permission_classes = (AllowAny,)
+
+    def send_confirmation_code(self, user):
+        confirmation_code = default_token_generator.make_token(user)
+        user.confirmation_code = confirmation_code
+        user.save()
+        return send_mail(
+            'Код подтверждения',
+            'Код подтверждения {0}'.format(confirmation_code),
+            recipient_list=[user.email],
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            fail_silently=False,
+        )
+
+    def post(self, request):
+        serializer = SignUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        try:
+            user, created = User.objects.get_or_create(
+                username=serializer.validated_data['username'],
+                email=serializer.validated_data['email'],
+            )
+            if not created:
+                if user.is_activated:
+                    return Response(
+                        'Пользователь уже активирован.',
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+                else:
+                    self.send_confirmation_code(user)
+                    return Response(
+                        'Новый код подтверждения отправлен на вашу почту.',
+                        status=status.HTTP_200_OK,
+                    )
+        except IntegrityError:
+            return Response(
+                'Имя пользователя или email уже заняты.',
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        self.send_confirmation_code(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
